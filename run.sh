82ab5fd2a866f48cb706e3568ab0344f804f6361
dazanestor <dazanestor@gmail.com>
2025-01-07 23:34:34 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
-if [[ $ALLOW_REMOTE_ACCESS == "yes" ]];then
+#!/bin/bash
+
+# Configura las banderas adicionales si se permite el acceso remoto
+if [[ $ALLOW_REMOTE_ACCESS == "yes" ]]; then
     EXTRA_FLAGS="$EXTRA_FLAGS --bind-all"
 fi
 
-/opt/acestream/start-engine --client-console --http-port $HTTP_PORT $EXTRA_FLAGS
+# Ejecuta el motor AceStream
+/opt/acestream/start-engine --client-console --http-port $HTTP_PORT $EXTRA_FLAGS &
+
+# Ejecuta runsvdir como el proceso principal
+exec runsvdir /app
