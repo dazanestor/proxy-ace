a39071c9be600bb224060d0c31404fe17d8a47db
dazanestor <dazanestor@gmail.com>
2025-01-08 08:09:34 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
-
-# Configura las banderas adicionales si se permite el acceso remoto
-if [[ $ALLOW_REMOTE_ACCESS == "yes" ]]; then
+if [[ $ALLOW_REMOTE_ACCESS == "yes" ]];then
     EXTRA_FLAGS="$EXTRA_FLAGS --bind-all"
 fi
-
-# Ejecuta el motor AceStream
-/opt/acestream/start-engine --client-console --http-port $HTTP_PORT $EXTRA_FLAGS &
-
-# Inicia el servicio cron para gestionar tareas programadas
-service cron start
-
-# Ejecuta runsvdir como un proceso adicional
-runsvdir /app &
-
-# Inicia Apache como proceso principal
-exec apachectl -D FOREGROUND
+/opt/acestream/start-engine --client-console --http-port $HTTP_PORT $EXTRA_FLAGS
